e3ce1e7213991aa1958d43de6aadd186d4e3c8a4
Tyedmers Gerard (U109281) <gerard.tyedmers@mobi.ch>
2020-08-24 13:31:11 +0200

    cleanup in page transitions and svgs

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 /*jslint devel: true, browser: true */ /*global  self  */
 
 var CACHE_NAME = "grrds-dice-cache";
-var CACHE_VERSION = "v2.6";
+var CACHE_VERSION = "v2.7";
 var CACHE = CACHE_NAME + "-" + CACHE_VERSION;
 
 var urlsToCache = [
